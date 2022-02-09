abfad61a62e88966e9741e76779479eeb080e787
Sergey <jaxi@atma.company>
2022-02-09 13:06:48 +0500

    Сделал отображение изображение с сервера в анамнез листе

--- src/components/Widgets/Anamnez/AnamnezCardItem.js
+++ src/components/Widgets/Anamnez/AnamnezCardItem.js
@@ -1,35 +1,89 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import React, { Component, useEffect, useState } from 'react';
+import { Text, View, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native';
 import baseURL from '../../../requests/baseURL'
+import ImageView from "react-native-image-viewing";
+import uuid from 'react-native-uuid';
 
 const AnamnezCardItem = ({ item }) => {
 
+    const imageDataList = []
+
     const itemsToRender = JSON.parse(item.value)
+    const [showViewer, setShowViewer] = useState(false)
+    const [imageList, setImageList] = useState(imageDataList)
+    const [currentIndex, setCurrentIndex] = useState()
+
+    useEffect(() => {
+        itemsToRender.file && itemsToRender.file.split(';').forEach(element => {
+            imageDataList.push(
+                { uri: baseURL + "u/i/" + element }
+            )
+        });
+
+        setImageList(imageDataList)
+    }, [])
 
     return (
         <View style={ styles.mainContent }>
-            <Text style={{ color: '#434A53', fontSize: 17, fontWeight: '700' }}>
-                { item.title }
-            </Text>
             {
-                itemsToRender["bool"] ? 
-                    (
-                        itemsToRender["bool"] == "Нет" ?
-                            <Text style={ styles.belowtextStyle }>Нет</Text> : 
-                            <Text style={ styles.belowtextStyle }>Да, { itemsToRender.val }</Text>
-                    ) : 
-                itemsToRender["choices"] ?
-                    (
-                        itemsToRender["choices"].map((item) => {
-                            return (
-                                <Text style={ styles.belowtextStyle }>{ "- " + item }</Text>
+                showViewer ? (
+                    <ImageView 
+                        images={imageList}
+                        imageIndex={currentIndex}
+                        visible={showViewer}
+                        swipeToCloseEnabled={false}
+                        onRequestClose={() => setShowViewer(false)}
+                    />
+                ) : 
+                (
+                    <View>
+                        <Text style={{ color: '#434A53', fontSize: 17, fontWeight: '700' }}>
+                            { item.title }
+                        </Text>
+                        {
+                            itemsToRender["bool"] ? 
+                                (
+                                    itemsToRender["bool"] == "Нет" ?
+                                        <Text style={ styles.belowtextStyle }>Нет</Text> : 
+                                        <Text style={ styles.belowtextStyle }>Да, { itemsToRender.val }</Text>
+                                ) : 
+                            itemsToRender["choices"] ?
+                                (
+                                    itemsToRender["choices"].map((item) => {
+                                        return (
+                                            <Text style={ styles.belowtextStyle }>{ "- " + item }</Text>
+                                        )
+                                    })
+                                ) : 
+                            ( <Text style={ styles.belowtextStyle }>{ itemsToRender.val }</Text> ,
+                                itemsToRender.file && 
+                                <View style={{ width: '100%', marginTop: 10 }}>
+                                    {
+                                        <FlatList 
+                                            style={{
+                                                width: '100%',
+                                            }}
+                                            horizontal={true}
+                                            data={itemsToRender.file.split(";")}
+                                            keyExtractor={(index) => index}
+                                            renderItem={({ item, index }) => {
+                                                return(
+                                                    <TouchableOpacity 
+                                                        onPress={() => { setCurrentIndex(index), setShowViewer(true) }} 
+                                                        style={{ marginRight: 5 }}
+                                                    >
+                                                        <Image 
+                                                            source={{ uri: baseURL + "u/i/" + item }} 
+                                                            style={{ width: 150, height: 150, borderRadius: 12 }} />
+                                                    </TouchableOpacity> 
+                                                )
+                                            }}
+                                        />
+                                    }
+                                </View>
                             )
-                        })
-                    ) : 
-                ( <Text style={ styles.belowtextStyle }>{ itemsToRender.val }</Text> ,
-                    itemsToRender.file && itemsToRender.file.split(";").map((item) => (
-                        <Image source={{ uri: baseURL + "u/i/" + item }} style={{ width: 150, height: 150 }} />
-                    )) 
+                        }
+                    </View>
                 )
             }
         </View>
